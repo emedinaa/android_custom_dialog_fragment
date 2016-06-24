bbdcc0d85cdb3f0be1ae4fcc6eac2b6c23f1e2e3
Eduardo Medina <emedinaa@gmail.com>
2016-06-24 10:34:21 -0500

    clean

--- SampleCustomDialog/app/src/main/java/com/emedinaa/android/customdialog/fragments/CustomDialogFragment.java
+++ SampleCustomDialog/app/src/main/java/com/emedinaa/android/customdialog/fragments/CustomDialogFragment.java
@@ -15,19 +15,10 @@ import com.emedinaa.android.customdialog.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CustomDialogFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class CustomDialogFragment extends DialogFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    
     private CustomDialogListener mListener;
 
     public CustomDialogFragment() {
